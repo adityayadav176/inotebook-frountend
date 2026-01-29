1c5ee72aa4ec4e04dfda4cbd66db0367f2116668
adityayadav176 <ay6598aditya@gmail.com>
2026-01-30 00:19:59 +0530

    Build Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <>
-    Hy my name is aditya
+     <Router>
+  <Navbar/>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+  </Routes>
+</Router>
+
     </>
   );
 }
