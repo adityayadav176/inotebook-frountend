1b7003e0e5c1a1c8ba7a6037c6daf4fd906fbca8
adityayadav176 <ay6598aditya@gmail.com>
2026-01-30 21:46:35 +0530

    Adding noteItem in a seprate Note Components

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -6,7 +6,8 @@ const NoteItem = (props) => {
         <div className='col-md-3'>
             <div class="card my-3">
                 <div class="card-body">
-                    
+                    <h5 class="card-title">{note.title}</h5>
+                    <p class="card-text">  {note.description}</p>
                 </div>
             </div>
         </div>
