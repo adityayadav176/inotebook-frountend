767cf9d2c0c5d1284a355245da08b8b4b479fe6e
adityayadav176 <ay6598aditya@gmail.com>
2026-01-30 21:45:32 +0530

    Fetching all notes from context

---
+++ src/components/NoteItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NoteItem = (props) => {
+    const { note } = props;
+    return (
+        <div className='col-md-3'>
+            <div class="card my-3">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NoteItem
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,9 +1,83 @@
 import NoteContext from "./noteContext";
-import { useState } from "react";
+import { useState, setNotes } from "react";
 
 const NoteState = (props) =>{
+    const notes = [
+  {
+    "_id": "697cbd81f59e5a5b0ae76664",
+    "user": "697b2db9d73bc0e90fd8e925",
+    "title": "instagram",
+    "description": "instagram is dangeros for children",
+    "tag": "personal",
+    "date": "2026-01-30T14:17:37.310Z",
+    "__v": 0
+  },
+  {
+    "_id": "697cbd81f59e5a5b0ae76666",
+    "user": "697b2db9d73bc0e90fd8e925",
+    "title": "instagram",
+    "description": "instagram is dangeros for children",
+    "tag": "personal",
+    "date": "2026-01-30T14:17:37.520Z",
+    "__v": 0
+  },
+   {
+    "_id": "697cbd81f59e5a5b0ae76664",
+    "user": "697b2db9d73bc0e90fd8e925",
+    "title": "instagram",
+    "description": "instagram is dangeros for children",
+    "tag": "personal",
+    "date": "2026-01-30T14:17:37.310Z",
+    "__v": 0
+  },
+  {
+    "_id": "697cbd81f59e5a5b0ae76666",
+    "user": "697b2db9d73bc0e90fd8e925",
+    "title": "instagram",
+    "description": "instagram is dangeros for children",
+    "tag": "personal",
+    "date": "2026-01-30T14:17:37.520Z",
+    "__v": 0
+  },
+   {
+    "_id": "697cbd81f59e5a5b0ae76664",
+    "user": "697b2db9d73bc0e90fd8e925",
+    "title": "instagram",
+    "description": "instagram is dangeros for children",
+    "tag": "personal",
+    "date": "2026-01-30T14:17:37.310Z",
+    "__v": 0
+  },
+  {
+    "_id": "697cbd81f59e5a5b0ae76666",
+    "user": "697b2db9d73bc0e90fd8e925",
+    "title": "instagram",
+    "description": "instagram is dangeros for children",
+    "tag": "personal",
+    "date": "2026-01-30T14:17:37.520Z",
+    "__v": 0
+  },
+   {
+    "_id": "697cbd81f59e5a5b0ae76664",
+    "user": "697b2db9d73bc0e90fd8e925",
+    "title": "instagram",
+    "description": "instagram is dangeros for children",
+    "tag": "personal",
+    "date": "2026-01-30T14:17:37.310Z",
+    "__v": 0
+  },
+  {
+    "_id": "697cbd81f59e5a5b0ae76666",
+    "user": "697b2db9d73bc0e90fd8e925",
+    "title": "instagram",
+    "description": "instagram is dangeros for children",
+    "tag": "personal",
+    "date": "2026-01-30T14:17:37.520Z",
+    "__v": 0
+  }
+]
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
              {props.children}
         </NoteContext.Provider>
     )
