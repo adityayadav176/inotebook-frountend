c717ca8fabd41348f118661221dab3346ef9105e
adityayadav176 <ay6598aditya@gmail.com>
2026-02-01 20:31:18 +0530

    Adding fetch note functionality to inotebook

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,25 +1,76 @@
-import  React, {useContext, useEffect} from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import noteContext from '../context/notes/noteContext';
 import NoteItem from './NoteItem';
 import AddNote from './NotesAdd';
 
 const Notes = () => {
-    const context = useContext(noteContext);
+  const context = useContext(noteContext);
   const { notes, getNotes } = context;
   useEffect(() => {
-   getNotes()
+    getNotes()
   }, [])
-  
+  const ref = useRef(null)
+   const [note, setNote] = useState({etitle:"", eDescription:"", etag:""})
+
+  const updateNote = (currentNote) => {
+    ref.current.click();
+    setNote({etitle: currentNote.title, eDescription: currentNote.description, etag: currentNote.tag})
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("Updating The Note ", note)
+  }
+
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })
+  }
+
   return (
     <>
-    <AddNote/>
+      <AddNote />
+      <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Edit Note</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div className="modal-body">
+              <form className='my-3'>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">Title</label>
+                  <input type="text" className="form-control" id="etitle" name='etitle' aria-describedby="emailHelp" value={note.etitle} onChange={onChange} />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="Description" className="form-label">Description</label>
+                  <input type="text" className="form-control" id="eDescription" name='eDescription' value={note.eDescription} onChange={onChange} />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="tag" className="form-label">Tag</label>
+                  <input type="text" className="form-control" id="etag" name='etag' value={note.etag} onChange={onChange} />
+                </div>
+              </form>
+            </div>
+
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" onClick={handleClick} className="btn btn-primary">Update note</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="row my-3">
         <h2>Your Notes</h2>
-        {notes.map((note)=>{
-          return <NoteItem key={note._id} note={note}/>
+        {notes.map((note) => {
+          return <NoteItem key={note._id} updateNote={updateNote} note={note} />
         })}
       </div>
-      </>
+    </>
   )
 }
 
