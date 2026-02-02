6b5d57f001817c2b2a835271242bc76a0a64e345
adityayadav176 <ay6598aditya@gmail.com>
2026-02-02 18:57:32 +0530

    Adding signup components to inotebook

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+  const [credentials, setCredentials] = useState({name: "", email: "", password: "", cpassword: ""});
+     let navigate = useNavigate();
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        const {name, email, password} = credentials;
+          const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+      
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({name, email, password})
+        });
+        const json = await response.json()
+        console.log(json);
+        if (json.success){
+            // redirect
+            localStorage.setItem('token', json.authtoken);
+            navigate("/")
+        }
+        else{
+            alert("Invalid Credentials");
+        }
+
+    }
+      const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
   return (
     <div>
-        <h1>I am a Signup component</h1>
+      <form onSubmit={handleSubmit}  autoComplete="new-password">
+          <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input type="name" className="form-control" id="name" name='name' aria-describedby="emailHelp" onChange={onChange}/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="email" name='email'  autoComplete="new-password" aria-describedby="emailHelp" onChange={onChange}/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="Password" name='password'  autoComplete="new-password" onChange={onChange} minLength={8} required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cPassword" className="form-label">Confirm Password</label>
+          <input type="cpassword" className="form-control" id="cPassword" name='cpassword' onChange={onChange} minLength={8}  required/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   )
 }
